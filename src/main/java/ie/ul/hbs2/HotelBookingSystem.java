e64f58f2711a994e86f3c9018a3d00153a6870ca
IANS-LENOVO\Ian <16151135@studentmail.ul.ie>
2019-11-23 12:56:14 +0000

    Removing error details and small fixes in management

--- src/main/java/ie/ul/hbs2/HotelBookingSystem.java
+++ src/main/java/ie/ul/hbs2/HotelBookingSystem.java
@@ -36,7 +36,7 @@ public class HotelBookingSystem {
         //payment test
         //bs.summary("test", "123", "25/09/2019", "17/10/2019", "3", "double", 15, 1000);
 
-        this.frame.show(search);
+        this.frame.show(management);
         //this.frame.show(booking);
        //this.frame.show(management);
 
